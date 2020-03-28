704662ad4e385ed5da2f0191688e49e6a84df844
Francisco Santana <francisco.filho@benner.com.br>
2020-03-27 23:48:25 -0300

    frontend da aplicação

---
+++ frontendd/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Routes from './routs';
+
+/**
+ * Conceito JSX = JavaScript XML (html integrado com js)
+ * Componente = função que retorna HTML
+ */
+
+function App() {  
+  return (
+    <Routes />
+  );
+}
+
+export default App;
